d7eb14b94f35870dc50e0465a5e289fd23f89b49
AxelRSa <axelg.rizos@gmail.com>
2022-08-01 15:46:11 -0500

    feat:
    modified:
    services/auth.services.js - removing login service, because nuxt/auth

--- services/auth.services.js
+++ services/auth.services.js
@@ -12,20 +12,6 @@ const singup = async (form) => {
   return data
 }
 
-const login = async (form) => {
-  const url = `${process.env.END_POINT}auth/login`
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(form)
-  })
-  const data = await response.json()
-  return data
-}
-
 export {
-  singup,
-  login
+  singup
 }
