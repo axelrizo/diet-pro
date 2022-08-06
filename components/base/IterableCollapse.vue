e6a08eee12b1725b97a811c8757eb9fb6e62973c
AxelRSa <axelg.rizos@gmail.com>
2022-08-06 14:56:43 -0500

    feat: layout of "food/all" view
    new file:
    components/base/IterableCollapse.vue - create new component to reuse, a collapse with 2 slots for the button and the content
    pages/food/all.vue - fisrt static layout of this page

---
+++ components/base/IterableCollapse.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+div.collapse-container
+  div(@click="collapseVisible=!collapseVisible")
+    slot(name="button")
+      b-button( block size="lg" variant="primary") Default button
+  b-collapse(:visible="collapseVisible")
+    slot(name="content")
+      p Default Content
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      collapseVisible: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.collapse-container{
+  border: solid 1px gray;
+  border-radius: .25rem;
+}
+</style>
