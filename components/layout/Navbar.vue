5d1ac8619c69602b9d088e4d28fbb7e7f565f5d1
AxelRSa <axelg.rizos@gmail.com>
2022-08-06 18:38:18 -0500

    feat: changing navbar links
    modified:
    components/layout/Navbar.vue - change links

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -1,15 +1,14 @@
 <template lang="pug">
 b-navbar(toggleable="lg" type="dark" variant="success")
   b-container
-    b-navbar-brand(href="#") Diet Pro
+    b-navbar-brand(to="/") Diet Pro
     b-navbar-toggle(target="nav-collapse")
     b-collapse#nav-collapse(is-nav="")
       b-navbar-nav.ml-auto
-        b-nav-item(href="#") Dashboard
-        b-nav-item(href="#") Users
-        b-nav-item(href="#") Plans
-        b-nav-item(href="#") Days
-        b-nav-item(href="#") Dishes
-        b-nav-item(href="#") Foods
+        b-nav-item(to="/") Users
+        b-nav-item(href="/plan/all") Plans
+        b-nav-item(href="/day/all") Days
+        b-nav-item(href="/meal/all") Meals
+        b-nav-item(href="/food/all") Foods
         b-nav-item(@click="$auth.logout()") Log out
 </template>
